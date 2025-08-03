f2dd2085f3926a1b83880aeb15ee66f738b349fd
shallow157 <shallowyang157@gmail.com>
2025-08-03 18:09:40 +0800

    Fix mobile and book content issues: rename markdown files to avoid encoding problems, add mobile viewport meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ThemeProvider } from '@/contexts/ThemeContext'
 import { LanguageProvider } from '@/contexts/LanguageContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
-      <LanguageProvider>
-        <Component {...pageProps} />
-      </LanguageProvider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="theme-color" content="#3B82F6" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </Head>
+      <ThemeProvider>
+        <LanguageProvider>
+          <Component {...pageProps} />
+        </LanguageProvider>
+      </ThemeProvider>
+    </>
   )
 }
 
